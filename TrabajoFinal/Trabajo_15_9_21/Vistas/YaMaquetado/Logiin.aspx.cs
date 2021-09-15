18043dca599d11137908f09931dcf73fbcac2615
Entropia-dev <54455913+Entropia-dev@users.noreply.github.com>
2021-09-15 07:17:15 -0300

    Avance en el loguin maquetado
    El loguin ya chequea los campos cuando se le da a loguear.

---
+++ TrabajoFinal/Trabajo_15_9_21/Vistas/YaMaquetado/Logiin.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+
+namespace Vistas.YaMaquetado
+{
+    public partial class Cuentaa : System.Web.UI.Page
+    {
+        NegocioCuenta neg = new NegocioCuenta();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+            Boolean estado = false;
+            estado = neg.validarCuenta(txtAlias.Text, txtContrasenia.Text);
+            if (estado == true)
+            {
+                Session["Alias"] = txtAlias.Text;
+              
+
+                Response.Redirect("Cuenta.aspx");
+            }
+            else
+            {
+
+                lblMensaje.Text = "No se pudo ingresar";
+
+            }
+        }
+    }
+}
